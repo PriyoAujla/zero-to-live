a47f4b5e67f224e87e20aa58b97388b7a9a0115d
Priyo Aujla <priyo.aujla@gmail.com>
2018-07-31 23:43:39 +0100

    Work in progress for episode 1.

---
+++ yet-another-social-media-app/src/test/kotlin/com/priyoaujla/app/HelloWorldTest.kt
@@ -0,0 +1,12 @@
+package com.priyoaujla.app
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class HelloWorldTest {
+
+    @Test
+    fun `test`() {
+        assertEquals("hello world", "hello world")
+    }
+}
